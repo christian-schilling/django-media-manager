aa6c045ea83d4d8e541321d049642cb601eefa22
Christian Schilling <christian@initcrash.net>
2009-06-01 22:00:55 +0200

    fixed setting variable name

--- management/commands/syncmedia.py
+++ management/commands/syncmedia.py
@@ -5,12 +5,9 @@ class Command(BaseCommand):
     def handle(self, **kwargs):
         from django.conf import settings
         import os
-        if not hasattr(settings,"SITE_MEDIA_ROOT"):
-            return
-        for appname,mediadir in media_dirs(settings.INSTALLED_APPS):
-            if os.path.isdir(os.path.join(mediadir,appname)):
-                os.system("rsync -avc %s/ %s" % (mediadir,settings.SITE_MEDIA_ROOT))
-            else:
-                os.system("rsync -avc %s/ %s" % (mediadir,os.path.join(settings.SITE_MEDIA_ROOT,appname)))
-
-
+        if hasattr(settings,"APP_MEDIA_ROOT"):
+            for appname,mediadir in media_dirs(settings.INSTALLED_APPS):
+                if os.path.isdir(os.path.join(mediadir,appname)):
+                    os.system("rsync -avc %s/ %s" % (mediadir,settings.APP_MEDIA_ROOT))
+                else:
+                    os.system("rsync -avc %s/ %s" % (mediadir,os.path.join(settings.APP_MEDIA_ROOT,appname)))
